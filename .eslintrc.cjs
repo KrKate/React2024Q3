1f4e99563e1ccbd51658c29bbe57ef87afc5fed1
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-07-08 14:29:08 +0500

    fix: add no-console rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -41,6 +41,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    "no-console": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
